5545876b1e807166a8f009290851258c2d6525d6
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-10-30 17:04:47 -0400

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1281,7 +1281,7 @@ class PlayState extends MusicBeatState
 				songSpeed = ClientPrefs.getGameplaySetting('scrollspeed');
 		}
 
-		var songData = SONG;
+		var songData: SwagSong = SONG;
 		Conductor.bpm = songData.bpm;
 
 		curSong = songData.song;
