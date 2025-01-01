952b70a796b9a79ab9fa4ba33e531bdb290c43ed
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-01-01 03:56:32 -0500

    fix loadsong

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -396,15 +396,13 @@ class FunkinLua {
 			luaTrace("removeHScript: HScript is not supported on this platform!", false, false, FlxColor.RED);
 			#end
 		});
-
-		Lua_helper.add_callback(lua, "loadSong", function(?name:String = null, ?difficultyNum:Int = -1) {
-			if(name == null || name.length < 1)
-				name = Song.loadedSongName;
-			if (difficultyNum == -1)
-				difficultyNum = PlayState.storyDifficulty;
+		Lua_helper.add_callback(lua, "loadSong", function(?name:String, difficultyNum:Int = -1, ?difficultyArray:Array<String>) {
+			if (difficultyArray != null) Difficulty.list = difficultyArray; // 😎
+			if (name == null || name.length < 1) name = PlayState.SONG.song;
+			if (difficultyNum == -1) difficultyNum = PlayState.storyDifficulty;
 
 			var poop = Highscore.formatSong(name, difficultyNum);
-			Song.loadFromJson(poop, name);
+			PlayState.SONG = Song.loadFromJson(poop, name);
 			PlayState.storyDifficulty = difficultyNum;
 			FlxG.state.persistentUpdate = false;
 			LoadingState.loadAndSwitchState(new PlayState());
